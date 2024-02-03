851ad6f2e32dd396c080df3c1af734f24a6ee5f5
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-02-03 17:33:17 +0100

    update xmake modules+remove validation layers

--- xmake.lua
+++ xmake.lua
@@ -1,23 +1,22 @@
 add_rules("mode.debug", "mode.release")
 
-option("validation_layers")
-option_end()
 
-add_requires("vulkan-headers 1.3.268", "vulkan-hpp 1.3.268", "moltenvk", "glm")
-if has_config("validation_layers") then 
-    add_requires("vulkan-validationlayers")
-end 
+add_requires("vulkan-headers 1.3.268", "glm")
+if is_plat("macosx") then 
+    add_requires("moltenvk")
+end
 
 target("glvk")
     set_kind("$(kind)")
     set_languages("cxx23")
-    add_files(
-        "src/*.cpp", 
-        "modules/**.mpp", 
-        "stdmodules/std.cppm"
-    )
-    add_packages("vulkan-headers", "vulkan-hpp", "moltenvk", "glm")
+    for _, paths in ipairs({"src/*.cpp", "modules/**.mpp"}) do
+        for _, file in ipairs(os.files(paths)) do
+            add_files(file)
+        end
+    end
+    if is_plat("macosx") then 
+        add_packages("moltenvk")
+    end
+    add_packages("vulkan-headers", "glm")
     -- for std modules
-    add_cxxflags("-stdlib=libc++", "-fexperimental-library")
-    set_policy("build.c++.modules", true)
-    set_policy("build.c++.clang.stdmodules", false)
+    add_cxxflags("-fexperimental-library")
